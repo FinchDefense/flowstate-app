9771b63d20948f959a623600bb5d96774784d635
Jerry Yang <87034558+FinchDefense@users.noreply.github.com>
2026-08-01 15:17:10 -0400

    Layout the basics of the Timer Component

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,3 +1,16 @@
+import './Timer.css'
+
 export function Timer() {
-  return <div></div>;
+  return (
+    <div className="timer-container">
+      <div className="timer-display">
+        <div className="timer-display-time"></div>
+      </div>
+      <div className="timer-buttons">
+        <button>Start</button>
+        <button>Reset</button>
+        <button>Stop</button>
+      </div>
+    </div>
+  );
 }
