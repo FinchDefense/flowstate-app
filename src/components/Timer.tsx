995c84ebd7eb78ddd12ae84458b47e70957cfaa2
Jerry Yang <87034558+FinchDefense@users.noreply.github.com>
2026-08-02 11:27:08 -0400

    Add time option buttons

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import './Timer.css'
 
 export function Timer() {
+  const [time, setTime] = useState<number>()
+
   return (
     <div className="timer-container">
       <div className="timer-display">
@@ -18,6 +21,14 @@ export function Timer() {
           <button>- 5 </button>
         </div>
       </div>
+      <div className="timer-buttons-time-options">
+          <button>5m</button>
+          <button>15m</button>
+          <button>25m</button>
+          <button>30m</button>
+          <button>45m</button>
+          <button>60m</button>
+        </div>
     </div>
   );
 }
