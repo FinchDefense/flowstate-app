e97b704a8fe4de82cf2d1ddbcfcdd21d90f4e65a
Jerry Yang <87034558+FinchDefense@users.noreply.github.com>
2026-08-02 18:11:01 -0400

    Update time value that is displayed when the relevant buttons are clicked

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,12 +2,21 @@ import { useState } from 'react';
 import './Timer.css'
 
 export function Timer() {
-  const [time, setTime] = useState<number>()
+  const [time, setTime] = useState<number>(1500);
+
+  const formatTime = (totalSeconds: number): string => {
+    const minutes = Math.floor(totalSeconds/60);
+    const seconds = totalSeconds % 60;
+    const paddedMinutes = String(minutes).padStart(2, '0');
+    const paddedSeconds = String(seconds).padStart(2, '0');
+
+    return `${paddedMinutes}:${paddedSeconds}`;
+  }
 
   return (
     <div className="timer-container">
       <div className="timer-display">
-        <div className="timer-display-time">25:00</div>
+        <div className="timer-display-time">{formatTime(time)}</div>
       </div>
       <div className="timer-buttons">
         <div className="timer-buttons-main-controls">
@@ -22,12 +31,12 @@ export function Timer() {
         </div>
       </div>
       <div className="timer-buttons-time-options">
-          <button>5m</button>
-          <button>15m</button>
-          <button>25m</button>
-          <button>30m</button>
-          <button>45m</button>
-          <button>60m</button>
+          <button onClick={() => setTime(300)}>5m</button>
+          <button onClick={() => setTime(900)}>15m</button>
+          <button onClick={() => setTime(1500)}>25m</button>
+          <button onClick={() => setTime(1800)}>30m</button>
+          <button onClick={() => setTime(2700)}>45m</button>
+          <button onClick={() => setTime(3600)}>60m</button>
         </div>
     </div>
   );
