091ab30fe9576bf6742a0242fa9897c9db0fdfa1
Jerry Yang <87034558+FinchDefense@users.noreply.github.com>
2026-08-02 11:18:22 -0400

    Add the basic css for the buttons in timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -4,12 +4,19 @@ export function Timer() {
   return (
     <div className="timer-container">
       <div className="timer-display">
-        <div className="timer-display-time"></div>
+        <div className="timer-display-time">25:00</div>
       </div>
       <div className="timer-buttons">
-        <button>▶ Start</button>
-        <button>⟳ Reset</button>
-        <button>⏹ Stop</button>
+        <div className="timer-buttons-main-controls">
+          <button>▶ Start</button>
+          <button>⏹ Stop</button>
+          <button>⟳ Reset</button>
+        </div>
+        <div className="timer-buttons-secondary-controls">
+          <button>⏭ Skip</button>
+          <button>+ 5</button>
+          <button>- 5 </button>
+        </div>
       </div>
     </div>
   );
