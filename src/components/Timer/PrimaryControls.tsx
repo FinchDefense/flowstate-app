e500b5bbb397f60cc0c63a2c8e844b010531aab6
Jerry Yang <87034558+FinchDefense@users.noreply.github.com>
2026-08-06 21:47:20 -0400

    Add an animation to the start/pause button so it fades in and out on click

--- src/components/Timer/PrimaryControls.tsx
+++ src/components/Timer/PrimaryControls.tsx
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+
 interface PrimaryControlsProps {
   handleStartPause: () => void;
   isRunning: boolean;
@@ -12,10 +14,29 @@ export function PrimaryControls({ handleStartPause, setInFocusMode, isRunning, t
       ? { icon: "🔄", text: "Restart" }
       : { icon: "▶", text: "Start" };
 
+  const buttonRef = useRef<HTMLButtonElement>(null); 
+
+  const animationOnClick = () => {
+    const button = buttonRef.current;
+    if (button) {
+      button.classList.add('animate-click');
+
+      const handleAnimationEnd = () => {
+        button.classList.remove('animate-click');
+        button.removeEventListener('animationend', handleAnimationEnd);
+      };
+
+      button.addEventListener('animationend', handleAnimationEnd);
+  }
+};
+
   return (
     <>
       <div className="timer-buttons-main-controls">
-        <button onClick={handleStartPause} className="start-pause-button">
+        <button 
+          onClick={() => {handleStartPause(); animationOnClick(); }} 
+          className="start-pause-button"
+          ref={buttonRef}>
           <span className="button-icon">{buttonContent.icon}</span>
           <span>{buttonContent.text}</span>
         </button>
