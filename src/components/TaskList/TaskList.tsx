9492d0ed600e393d5f8a506d2492d9debe25383e
Jerry Yang <87034558+FinchDefense@users.noreply.github.com>
2026-08-08 12:55:29 -0400

    Add the basic layout and css for tasklist: Header, input bar, and "Add" button

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,3 +1,17 @@
+import './TaskList.css'
+
 export function TaskList() {
-  return <div></div>;
+
+  return (
+    <div className="task-list-container">
+      <h1>📝 Task Tracker</h1>
+      <div className="task-row">
+        <input
+        placeholder="Add a new task..."
+        className="task-input"
+        />
+        <button className="add-task-button">Add</button>
+      </div>
+    </div>
+  )
 }
