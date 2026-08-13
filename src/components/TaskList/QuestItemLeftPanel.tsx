26d1b111637838e4c83cc18dec1ecc4f7e7cb9ed
Jerry Yang <87034558+FinchDefense@users.noreply.github.com>
2026-08-13 10:23:13 -0400

    Automatically update quest title to uppercase format when it appears on the left panel

--- src/components/TaskList/QuestItemLeftPanel.tsx
+++ src/components/TaskList/QuestItemLeftPanel.tsx
@@ -9,7 +9,7 @@ export function QuestItemLeftPanel({ quest }: QuestItemLeftPanelProps) {
   return (
     <div className="quest-item-container">
       <input type="checkbox"></input>
-      <span>{quest.title}</span>
+      <span>{quest.title.toUpperCase()}</span>
       <div className="tag-container">
         <span>{quest.campaign !== '' ? `[Campaign: ${quest.campaign}]` : "[Campaign: none]" }</span>
         <span>{quest.threatTier === 'Trivial' ? '🟢' : quest.threatTier === 'Guarded' ? '🛡️' : quest.threatTier === 'Perilous' ? '💀' : '⚪' }</span>
