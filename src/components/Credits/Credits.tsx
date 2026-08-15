eb8a35747625eae2755f7d5d50816ce057b91a93
Jerry Yang <87034558+FinchDefense@users.noreply.github.com>
2026-08-15 19:34:19 -0400

    feat(add a special thanks and disclaimer section and a scroll-down animation on mount)
    - Scroll from top of the screen to bottom on mount (movie credits-like) using a custom keyframes animation
    - Add a special thanks and disclaimer section, style those sections accordingly
    - Optimize spacing issues and layout

--- src/components/Credits/Credits.tsx
+++ src/components/Credits/Credits.tsx
@@ -36,6 +36,16 @@ export function Credits({ setShowCredits }: CreditsProps) {
             <h3>SuperSimpleDev</h3>
           </div>
         </div>
+
+        <div className="special-thanks-container">
+          <div className="special-thanks-title">SPECIAL THANKS</div>
+          <div className="special-thanks-text">Dedicated to early-stage supporters who advised and applauded the first instances of this realm. This is also in appreciation of the open-source educators, who walked the path and continue to inspire those tinkering and learning under the cover of darkness as they build their passion projects.</div>
+        </div>
+
+        <div className="disclaimer-container">
+          <div className="disclaimer-title">DISCLAIMER</div>
+          <div className="disclaimer-text">This application is a strictly non-commercial, educational fan project. All rights to the ambient music and original UI inspirations belong to CD Projekt Red, FromSoftware, and their respective composers and creators. No copyright infringement is intended.</div>
+        </div>
       </div>
     </div>
   );
